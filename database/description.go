0bce3f130fe8fe363158685015c1391f3f77a045
liuwill <liuwei_will@qq.com>
2019-08-21 12:10:32 +0800

    build marked table from table data

--- database/description.go
+++ database/description.go
@@ -1,12 +1,31 @@
 package database
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"strings"
+)
 
 const (
 	RAW_SCHEMA_CONFIG = `[
-		{ "column": "table_name", "name": "表名" },
+		{ "column": "table_name", "name": "表名", "anchor": true },
 		{ "column": "table_comment", "name": "描述" },
 		{ "column": "engine", "name": "存储引擎" }
+	]`
+	RAW_COLUMN_CONFIG = `[
+		{ "column": "column_name", "name": "列名" },
+		{ "column": "column_comment", "name": "描述" },
+		{ "column": "column_type", "name": "类型" },
+		{ "column": "is_nullable", "name": "允许为空" },
+		{ "column": "column_key", "name": "键类型" },
+		{ "column": "column_default", "name": "默认值" },
+		{ "column": "extra", "name": "额外属性" }
+	]`
+	RAW_INDEX_CONFIG = `[
+		{ "column": "index_name", "name": "索引名称" },
+		{ "column": "seq_in_index", "name": "键位置" },
+		{ "column": "column_name", "name": "列名" },
+		{ "column": "non_unique", "name": "是否唯一" },
+		{ "column": "cardinality", "name": "基数" }
 	]`
 )
 
@@ -20,4 +39,63 @@ func BuildColumnConfig(jsonStr string) []ColumnConfig {
 	columnConfig := []ColumnConfig{}
 	json.Unmarshal([]byte(jsonStr), &columnConfig)
 	return columnConfig
+}
+
+func ParseTableContent(jsonStr string) []map[string]interface{} {
+	var tableData []map[string]interface{}
+	json.Unmarshal([]byte(jsonStr), &tableData)
+	return tableData
+}
+
+func JoinMarkedLine(columns []string) string {
+	line := strings.Join(columns, " | ")
+	return "| " + line + " |"
+}
+
+func BuildMarkedTableHeader(tableConfig []ColumnConfig) (string, string) {
+	headerMeta := make([]string, len(tableConfig))
+	splitterMeta := make([]string, len(tableConfig))
+
+	for i, item := range tableConfig {
+		headerMeta[i] = item.Name
+		splitterMeta[i] = "--------:"
+	}
+
+	header := JoinMarkedLine(headerMeta)
+	splitter := JoinMarkedLine(splitterMeta)
+	return header, splitter
+}
+
+func FetchTableColumnValue(config ColumnConfig, rowData map[string]interface{}) string {
+	key := strings.ToUpper(config.Column)
+	columnVal := rowData[key].(string)
+	if config.IsAnchor {
+		columnVal = "[" + columnVal + "](#" + columnVal + ")"
+	}
+	return columnVal
+}
+
+func BuildMarkedTable(tableConfig []ColumnConfig, tableData []map[string]interface{}) []string {
+	header, splitter := BuildMarkedTableHeader(tableConfig)
+	lines := make([]string, len(tableData)+2)
+
+	pos := 0
+	tableHeader := []string{
+		header, splitter,
+	}
+	for _, val := range tableHeader {
+		lines[pos] = val
+		pos++
+	}
+
+	for _, current := range tableData {
+		lineItems := make([]string, len(tableConfig))
+		for j, config := range tableConfig {
+			lineItems[j] = FetchTableColumnValue(config, current)
+		}
+		lines[pos] = JoinMarkedLine(lineItems)
+		pos++
+	}
+
+	return lines
 }
--- database/description_test.go
+++ database/description_test.go
@@ -5,6 +5,57 @@ import (
 	"testing"
 )
 
+const schemaData = `[
+  {
+    "TABLE_CATALOG": "def",
+    "TABLE_SCHEMA": "pro_calshop",
+    "TABLE_NAME": "tb_admin",
+    "TABLE_TYPE": "BASE TABLE",
+    "ENGINE": "InnoDB",
+    "VERSION": 10,
+    "ROW_FORMAT": "Compact",
+    "TABLE_ROWS": 7,
+    "AVG_ROW_LENGTH": 2340,
+    "DATA_LENGTH": 16384,
+    "MAX_DATA_LENGTH": 0,
+    "INDEX_LENGTH": 0,
+    "DATA_FREE": 0,
+    "AUTO_INCREMENT": 8,
+    "CREATE_TIME": "2019-01-16 10:28:50",
+    "UPDATE_TIME": null,
+    "CHECK_TIME": null,
+    "TABLE_COLLATION": "utf8_general_ci",
+    "CHECKSUM": null,
+    "CREATE_OPTIONS": "",
+    "TABLE_COMMENT": "管理员信息表",
+    "BLOCK_FORMAT": "Original"
+  },
+  {
+    "TABLE_CATALOG": "def",
+    "TABLE_SCHEMA": "pro_calshop",
+    "TABLE_NAME": "tb_ads_position",
+    "TABLE_TYPE": "BASE TABLE",
+    "ENGINE": "InnoDB",
+    "VERSION": 10,
+    "ROW_FORMAT": "Compact",
+    "TABLE_ROWS": 11,
+    "AVG_ROW_LENGTH": 1489,
+    "DATA_LENGTH": 16384,
+    "MAX_DATA_LENGTH": 0,
+    "INDEX_LENGTH": 32768,
+    "DATA_FREE": 0,
+    "AUTO_INCREMENT": 12,
+    "CREATE_TIME": "2019-07-23 14:43:14",
+    "UPDATE_TIME": null,
+    "CHECK_TIME": null,
+    "TABLE_COLLATION": "utf8_general_ci",
+    "CHECKSUM": null,
+    "CREATE_OPTIONS": "",
+    "TABLE_COMMENT": "系统广告位表",
+    "BLOCK_FORMAT": "Original"
+  }
+]`
+
 func Test_BuildColumnConfig(t *testing.T) {
 	tableRows := BuildColumnConfig(RAW_SCHEMA_CONFIG)
 	count := strings.Count(RAW_SCHEMA_CONFIG, "column")
@@ -12,5 +63,28 @@ func Test_BuildColumnConfig(t *testing.T) {
 	if len(tableRows) != count {
 		t.Error("Test BuildColumnConfig Fail", len(tableRows), count)
 	}
+
 	t.Log("Test BuildColumnConfig Success")
+}
+
+func Test_ParseTableContent(t *testing.T) {
+	tableRows := ParseTableContent(RAW_SCHEMA_CONFIG)
+	count := strings.Count(RAW_SCHEMA_CONFIG, "column")
+
+	if len(tableRows) != count {
+		t.Error("Test BuildColumnConfig Fail", len(tableRows), count)
+	}
+	t.Log("Test BuildColumnConfig Success")
+}
+
+func Test_BuildMarkedTable(t *testing.T) {
+	tableConfig := BuildColumnConfig(RAW_SCHEMA_CONFIG)
+	tableData := ParseTableContent(schemaData)
+
+	markedLines := BuildMarkedTable(tableConfig, tableData)
+	if len(markedLines) != len(tableData)+2 {
+		t.Error("Test BuildMarkedTable Fail", len(markedLines), len(tableData))
+	}
+
+	t.Log("Test BuildMarkedTable Success")
 }
